c2052dd6b8195f68d28e27a7244e4e0f46fd6331
tgao17 <113127230+tgao17@users.noreply.github.com>
2023-07-16 11:35:43 -0700

    database model added

---
+++ server/src/models/dbModel.js
@@ -0,0 +1,15 @@
+const { Pool } = require('pg');
+
+const PG_URI =
+  'postgres://mqyenfhu:Q-XccTOehb_iYM3Qq4Xh3vXhYNPRF2Mm@mahmud.db.elephantsql.com/mqyenfhu';
+const pool = new Pool({
+  connectionString: PG_URI,
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('executed query', text);
+    console.log('query params', params);
+    return pool.query(text, params, callback);
+  },
+};
