45b9d63fdedef145ec9b60c04502199efdf383ec
tgao17 <113127230+tgao17@users.noreply.github.com>
2023-07-17 09:07:13 -0700

    Some more middlewares

--- server/src/controllers/postController.js
+++ server/src/controllers/postController.js
@@ -1,50 +1,93 @@
-
 const db = require('../config/profileSchema.js');
 
 const postController = {};
 
-
-
-postController.findPost = (req,res,next) => {
+postController.findPost = (req, res, next) => {
   // Get a post with req.params.id == postId
   // Attach to res.locals.postRequest;
 
   next();
-}
+};
 
-postController.makePost = (req,res,next) => {
-// An authorized user is posting
-// Get username from cookies/session
-// Get post from body
-// Add the post to the DB
+postController.makePost = async (req, res, next) => {
+  // An authorized user is posting
+  // Get username from cookies/session
+  const { username } = req.cookies;
+  // Get post from body
+  const {
+    tech,
+    typeReview,
+    typeAdvice,
+    typeCodeSnippet,
+    typeHelpOffer,
+    language,
+    title,
+    comment,
+    image,
+  } = req.body;
 
-  next();
-}
+  // retreive tech id, uploader id, and language id
+  // code
 
-postController.editPost = (req,res,next) => {
+  try {
+    // Add the post to the DB
+    db.query(
+      `INSERT INTO posts (title, tech, uploader, type_review, type_advice, type_code_snippet, type_help_offer, language, comment, image) 
+      VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)`,
+      [
+        title,
+        tech_id,
+        uploader_id,
+        typeReview,
+        typeAdvice,
+        typeCodeSnippet,
+        typeHelpOffer,
+        languageid,
+        comment,
+        image,
+      ]
+    );
+
+    return next();
+  } catch (err) {
+    return next('error');
+  }
+};
+
+postController.editPost = (req, res, next) => {
   // An authorized/authenticated user wants to edit the post saved to res.locals.postRequest.
   // Edit the post by database ID
 
   next();
+};
 
-}
-
-postController.deletePost = (req,res,next) => {
+postController.deletePost = (req, res, next) => {
   // An authorized/authenticated user wants to delete their post (res.locals.postRequest)
   // Delete the post from the database by databaseId.
   next();
-}
+};
 
-postController.findPostsByUser = (req,res,next) => {
+postController.findPostsByUser = async (req, res, next) => {
   // Using req.params.userId, generate res.locals.postlist
+  // const { username } = req.params;
+  // const text = ``;
+  // try {
+  //   const { rows } = await db.query(text, [username]);
+  //   // if rows length is zero then it does not exist
+  //   res.locals.postList = rows;
+  //   return next();
+  // } catch (err) {
+  //   return next({
+  //     log: 'Express error handler caught at postController.findPostsByUser',
+  //     message: { err: 'Unable to find posts by this user' },
+  //   });
+  // }
+};
 
+postController.findPostsByTech = (req, res, next) => {
   next();
-}
-
-postController.findPostsByTech = (req,res,next) => {
-  next();
-  //req.params.id 
+  //req.params.id
   // res.locals.postList
-}
+};
 
 module.exports = postController;
--- server/src/routes/postRouter.js
+++ server/src/routes/postRouter.js
@@ -6,15 +6,12 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
-
-
 // POSTS
 // Find all posts by a user /api/post/posts-by-user/USER_ID
-router.get('/posts-by-user/:id', postController.findPostsByUser, (req,res)=>{
-
+// @STEVER how will you know the user id here from front end? i assume you would only know their username. when that user name is passed down we can convert it to Id
+router.get('/posts-by-user/:id', postController.findPostsByUser, (req, res) => {
   res.status(200).json(res.locals.postList);
-})
-
+});
 
 // Search for posts with at '/api/post/search?keywords=KEYWORDS' using 'req.query.keywords' - STRETCH GOAL
 // router.get('/search', (req,res)=>{
@@ -23,36 +20,49 @@ router.get('/posts-by-user/:id', postController.findPostsByUser, (req,res)=>{
 // })
 
 // Look up a single post /api/post/POST_ID
-router.get('/:id', postController.findPost, (req,res)=>{
-
+router.get('/:id', postController.findPost, (req, res) => {
   res.status(200).json(res.locals.postRequest);
-})
+});
 
 // Update a single post
-router.put('/:id', postController.findPost, userController.authenticate, userController.authorizeEdit, postController.editPost, (req,res)=>{
-
-  res.status(200).send();
-})
+router.put(
+  '/:id',
+  postController.findPost,
+  userController.authenticate,
+  userController.authorizeEdit,
+  postController.editPost,
+  (req, res) => {
+    res.status(200).send();
+  }
+);
 
 // Delete a single post
-router.put('/:id', postController.findPost, userController.authenticate, userController.authorizeEdit, postController.deletePost, (req,res)=>{
-
-  res.status(200).send();
-})
-
+router.put(
+  '/:id',
+  postController.findPost,
+  userController.authenticate,
+  userController.authorizeEdit,
+  postController.deletePost,
+  (req, res) => {
+    res.status(200).send();
+  }
+);
 
 // Add new Post to the database
-router.post('/', userController.authenticate, postController.makePost, (req,res)=>{
-  console.log('ready to send all posts')
-  res.sendStatus(200);
-});
+router.post(
+  '/',
+  // userController.authenticate, // skipped for testing
+  postController.makePost,
+  (req, res) => {
+    console.log('ready to send all posts');
+    res.sendStatus(200);
+  }
+);
 
 // // Fetch all posts for an 'all posts' display that likely won't be used
 // router.get('/', (req,res)=>{
 
 //   res.status(200).json(res.locals.postList);
 // });
-
-
 
 module.exports = router;
