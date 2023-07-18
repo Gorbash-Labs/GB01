718d307a68ed7a9ced79dbfa3e8f9ecf3823c89d
peterzepf <ptrgzpf@gmail.com>
2023-07-18 17:14:50 -0400

    began splitting home component into more components; created reducers, actions, and context providers for Home.jsx

--- client/pages/Home.jsx
+++ client/pages/Home.jsx
@@ -1,13 +1,75 @@
-import React, { useState, useEffect } from 'react';
+import React, {
+  useState,
+  useReducer,
+  useEffect,
+  useContext,
+  createContext,
+} from 'react';
 import Navbar from '../components/Navbar.jsx';
+import Apicard from '../components/Apicard.jsx';
 import './Home.scss';
 import { useNavigate, useParams } from 'react-router-dom';
 
+const actions = {
+  NAME_INPUT: 'NAME_INPUT',
+  URL_INPUT: 'URL_INPUT',
+  IMAGE_URL_INPUT: 'IMAGE_URL_INPUT',
+  DESCRIPTION_INPUT: 'DESCRIPTION_INPUT',
+  SUBMIT: 'SUBMIT',
+  LOAD: 'LOAD',
+  EXIT: 'EXIT',
+};
+
+const overlayStateInit = {
+  visible: false,
+  loading: 'init',
+  apiName: '',
+  apiUrl: '',
+  apiDescription: '',
+  apiImageUrl: '',
+  apiData: [],
+};
+
+const overlayStateReducer = (state, action) => {
+  switch (action.type) {
+    case actions.NAME_INPUT: {
+      return { ...state, apiName: action.payload };
+    }
+    case actions.URL_INPUT: {
+      return { ...state, apiUrl: action.payload };
+    }
+    case actions.IMAGE_URL_INPUT: {
+      return { ...state, apiImageUrl: action.payload };
+    }
+    case actions.DESCRIPTION_INPUT: {
+      return { ...state, apiDescription: action.payload };
+    }
+    case actions.LOAD: {
+      return { ...state, apiData: action.payload };
+    }
+    case actions.SUBMIT: {
+      return { ...state, loading: 'submit' };
+    }
+    case actions.EXIT: {
+      return {
+        visible: false,
+        apiName: '',
+        apiUrl: '',
+        apiDescription: '',
+        apiImageUrl: '',
+        apiData: [],
+        loading: 'idle',
+      };
+    }
+    default:
+      return state;
+  }
+};
 
 const Home = () => {
   //overlay state for showing the form, set true to appear
   const [showOverlay, setShowOverlay] = useState(false);
- 
+
   //state for the form inputs
   const [apiName, setApiName] = useState('');
   const [apiURL, setApiURL] = useState('');
@@ -15,8 +77,15 @@ const Home = () => {
   const [apiImageURL, setApiImageURL] = useState('');
   const [apiData, setApiData] = useState([]);
 
+  const [overlayState, overlayDispatch] = useReducer(
+    overlayStateReducer,
+    overlayStateInit,
+  );
+  const OverlayDispatchContext = createContext();
+  const OverlayFormContext = createContext();
+
   const navigate = useNavigate();
-  
+
   const openOverlay = () => {
     setShowOverlay(true);
   };
@@ -27,7 +96,6 @@ const Home = () => {
     const senderTechId = e.target.id;
     const senderName = e.target.name;
     navigate(`/comments/${senderTechId}`); // received as route.params
-    //
   }
 
   const addAPI = async () => {
@@ -45,6 +113,7 @@ const Home = () => {
         body: JSON.stringify({
           name: apiName,
           link: apiURL,
+
           image: apiImageURL,
           typeApi: false,
           typeFramework: false,
@@ -80,21 +149,103 @@ const Home = () => {
     fetchData();
   }, []);
 
+  // loading state changed: making a fetch
+  useEffect(async () => {
+    const { loading, apiImageURL, apiDescription, apiName, apiUrl } =
+      overlayState;
+    switch (loading) {
+      case 'init':
+        {
+          try {
+            const response = await fetch('/api/tech', {
+              method: 'GET',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            });
+            const data = await response.json();
+            const newData = JSON.parse(JSON.stringify(data));
+            overlayDispatch({ type: actions.LOAD, payload: newData });
+          } catch (err) {}
+        }
+        break;
+
+      case 'submit':
+        {
+          try {
+            const response = await fetch('/api/tech', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                name: apiName,
+                link: apiURL,
+
+                image: apiImageURL,
+                typeApi: false,
+                typeFramework: false,
+                typeLibrary: false,
+                description: apiDescription,
+                keywords: ['maps'],
+              }),
+            });
+
+            const data = await response.json();
+            console.log('success');
+            console.log('data returned', data);
+            overlayDispatch({ type: actions.LOAD, payload: data });
+            overlayDispatch({ type: actions.EXIT });
+          } catch (err) {
+            console.log(err);
+          }
+        }
+        break;
+
+      default:
+        break;
+    }
+  }, [overlayState.loading]);
+
+  return (
+    <OverlayDispatchContext.Provider value={overlayDispatch}>
+      <OverlayFormContext.Provider value={overlayState}>
+        <Navbar />
+        <MainHeader
+          openOverlay={openOverlay}
+          addAPI={addAPI}
+          apiData={apiData}
+          showOverlay={showOverlay}
+          apiName={apiName}
+          setApiName={setApiName}
+          apiURL={apiURL}
+          setApiURL={setApiURL}
+          apiDescription={apiDescription}
+          setApiDescription={setApiDescription}
+          apiImageURL={apiImageURL}
+          setApiImageURL={setApiImageURL}
+        />
+        <ApisContainer apiData={apiData} comments={comments} />
+      </OverlayFormContext.Provider>
+    </OverlayDispatchContext.Provider>
+  );
+};
+
+const ApisContainer = ({ apiData, comments }) => {
   const renderBox = () => {
     return apiData.map((item, index) => {
       console.log(item);
-
       return (
-        <div className="box" key={index}>
-          <div className="image-container">
-            <img src={item.image_url} alt="Tech" className="api-image" />
+        <div className='box' key={index}>
+          <div className='image-container'>
+            <img src={item.image_url} alt='Tech' className='api-image' />
           </div>
-          <div className="api-content">
-            <a href={item.link} className="tech-item-name">
+          <div className='api-content'>
+            <a href={item.link} className='tech-item-name'>
               {item.name}
             </a>
             <p>{item.description}</p>
-            <div className="button-comment">
+            <div className='button-comment'>
               <button onClick={comments} id={item.tech_id}>
                 Posts
               </button>
@@ -104,102 +255,114 @@ const Home = () => {
       );
     });
   };
-
   return (
-    <div>
-      <Navbar />
+    <div className='one'>
+      <div className='scroll-container'>
+        <div className='grid-container'>{renderBox()}</div>
+      </div>
+    </div>
+  );
+};
 
-      <div className="main-header">
-        <div>
-          <div className="content">
-            <div className="home-top-all-content">
-              <div className="home-top-title-button">
-                <h2>Cohort: CTRI 17</h2>
-                <div>
-                  <img src="./logo.png"></img>
-                </div>
-                <div>
-                  <button className="button" onClick={openOverlay}>
-                    + ADD TECH
-                  </button>
-                </div>
+const MainHeader = ({
+  openOverlay,
+  showOverlay,
+  addAPI,
+  apiName,
+  setApiName,
+  apiURL,
+  setApiURL,
+  apiDescription,
+  setApiDescription,
+  apiImageURL,
+  setApiImageURL,
+}) => {
+  return (
+    <div className='main-header'>
+      <div>
+        <div className='content'>
+          <div className='home-top-all-content'>
+            <div className='home-top-title-button'>
+              <h2>Cohort: CTRI 17</h2> {/* Hard coded org name */}
+              <div>
+                <img src='./logo.png'></img>
               </div>
-              <div className="input-container">
-                <input
-                  type="text"
-                  className="input-bar-home"
-                  placeholder="Search APIs..."
-                />
+              <div>
+                <button className='button' onClick={openOverlay}>
+                  + ADD TECH
+                </button>
               </div>
             </div>
-            {showOverlay && (
-              <div className="overlay">
-                <div className="overlay-content">
-                  <div>
-                    <form>
-                      <div className="formGroup">
-                        <h2>Add Tech</h2>
-                        <input
-                          type="text"
-                          className="input-one"
-                          placeholder="Add API Name"
-                          value={apiName}
-                          onChange={(event) => {
-                            setApiName(event.target.value);
-                          }}
-                        />
+            <div className='input-container'>
+              <input
+                type='text'
+                className='input-bar-home'
+                placeholder='Search APIs...'
+              />
+            </div>
+          </div>
+          {showOverlay && (
+            <div className='overlay'>
+              <div className='overlay-content'>
+                <div>
+                  <form>
+                    <div className='formGroup'>
+                      <h2>Add Tech</h2>
+                      <input
+                        type='text'
+                        className='input-one'
+                        placeholder='Add API Name'
+                        value={apiName}
+                        onChange={event => {
+                          setApiName(event.target.value);
+                        }}
+                      />
 
-                        <input
-                          type="text"
-                          className="input-one"
-                          placeholder="Add API URL"
-                          value={apiURL}
-                          onChange={(event) => {
-                            setApiURL(event.target.value);
-                          }}
-                        />
-                        <textarea
-                          className="input-one"
-                          rows="3"
-                          maxLength="150"
-                          placeholder="Add Brief Description"
-                          value={apiDescription}
-                          onChange={(event) => {
-                            setApiDescription(event.target.value);
-                          }}
-                        />
-                        <input
-                          type="text"
-                          className="input-one"
-                          placeholder="Add Image URL"
-                          value={apiImageURL}
-                          onChange={(event) => {
-                            setApiImageURL(event.target.value);
-                          }}
-                        />
-                        <input
-                          type="file"
-                          className="input-one"
-                          accept="image/*"
-                        />
-                      </div>
+                      <input
+                        type='text'
+                        className='input-one'
+                        placeholder='Add API URL'
+                        value={apiURL}
+                        onChange={event => {
+                          setApiURL(event.target.value);
+                        }}
+                      />
+                      <textarea
+                        className='input-one'
+                        rows='3'
+                        maxLength='150'
+                        placeholder='Add Brief Description'
+                        value={apiDescription}
+                        onChange={event => {
+                          setApiDescription(event.target.value);
+                        }}
+                      />
+                      <input
+                        type='text'
+                        className='input-one'
+                        placeholder='Add Image URL'
+                        value={apiImageURL}
+                        onChange={event => {
+                          setApiImageURL(event.target.value);
+                        }}
+                      />
+                      <input
+                        type='file'
+                        className='input-one'
+                        accept='image/*'
+                      />
+                    </div>
 
-                      <div className="btn">
-                        <button className="login-button" onClick={addAPI}>
-                          Submit!
-                        </button>
-                      </div>
-                    </form>
-                  </div>
+                    <div className='btn'>
+                      <button className='login-button' onClick={addAPI}>
+                        Submit!
+                      </button>
+                    </div>
+                  </form>
                 </div>
               </div>
-            )}
-          </div>
-        </div>
-      </div>
-      <div className="one">
-        <div className="scroll-container">
-          <div className="grid-container">{renderBox()}</div>
+            </div>
+          )}
         </div>
       </div>
     </div>
