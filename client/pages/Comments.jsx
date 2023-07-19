442d9e7292b7e3aa3b4e135fc7658f104dd71c13
peterzepf <ptrgzpf@gmail.com>
2023-07-18 20:45:55 -0400

    set up initial state for reducer paradigm

--- client/pages/Comments.jsx
+++ client/pages/Comments.jsx
@@ -1,15 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 import { useParams } from 'react-router-dom';
 import './Comments.scss';
 import Navbar from '../components/Navbar.jsx';
 import HelperFunctions from '../helper-functions';
 
+const commentsPageStateInit = {
+  accordianIndexExpanded: null,
+  formVisible: false,
+  editorContent: '',
+  contentEntry: null,
+  techName: '',
+  techLink: '',
+  techDescription: '',
+  techImage: '',
+  imageFile: null,
+};
+
+
+
 const Comments = () => {
   //this is the state for the accordian, when the accordian is clicked it invokes an active index
   const [activeIndex, setActiveIndex] = useState(null);
 
-  //state overlay that is changed to true when the button is clicked in order to appear 
+  //state overlay that is changed to true when the button is clicked in order to appear
   const [showOverlay, setShowOverlay] = useState(false);
 
   //here are the states for the form to keep track of each input
@@ -21,8 +35,6 @@ const Comments = () => {
   const [techImage, setTechImage] = useState('');
   const [entry, setEntry] = useState();
   const [image, setImage] = useState();
-
-
 
   //from here we had starting typing out the states to handle the backend format but realized we did not have enough time so it is not connected/finished
   /*
@@ -61,11 +73,9 @@ const Comments = () => {
 
   // comment VARCHAR(5000) NOT NULL,
 
-
   // language INTEGER NOT NULL,
   const [languageEntry, setLanguageEntry] = useState();
   const [commentEntries, setCommentEntries] = useState([]);
-
 
   //to find id of our url
   const { id } = useParams();
@@ -121,6 +131,7 @@ const Comments = () => {
         const data = await response.json();
         const newData = JSON.parse(JSON.stringify(data));
         // newData =  {tech: tech-obj, posts: [post-obj, post-obj, ..]}
+        console.log('Hello there hello there');
         setCommentEntries(newData.posts);
         setCurrentTech(newData.tech);
         setTechName(newData.tech.name);
@@ -133,12 +144,12 @@ const Comments = () => {
     fetchData();
   }, []);
 
-  const openOverlay = (e) => {
+  const openOverlay = e => {
     // e.preventDefault();
     setShowOverlay(true);
   };
 
-  const handleAccordionClick = (index) => {
+  const handleAccordionClick = index => {
     setActiveIndex(index === activeIndex ? null : index);
   };
 
@@ -151,27 +162,25 @@ const Comments = () => {
     return (
       <div
         key={index}
-        className={`accordion-item ${index === activeIndex ? 'active' : ''}`}
-      >
-        <div className="accordion-header-outer">
+        className={`accordion-item ${index === activeIndex ? 'active' : ''}`}>
+        <div className='accordion-header-outer'>
           <div
-            className="accordion-header"
-            onClick={() => handleAccordionClick(index)}
-          >
+            className='accordion-header'
+            onClick={() => handleAccordionClick(index)}>
             <div>{item.title}</div>
-            <div className="details">
-              <p className="username">{item.username}</p>
-              <p className="tags">Posted by: Steve</p>
+            <div className='details'>
+              <p className='username'>{item.username}</p>
+              <p className='tags'>Posted by: Steve</p>
             </div>
           </div>
         </div>
         {index === activeIndex && (
-          <div className="accordion-content">
+          <div className='accordion-content'>
             <div>
-              <div className="experience">
+              <div className='experience'>
                 {HelperFunctions.md(item.comment)}
               </div>
-              <img src={item.image} alt="Image" className="accordion-image" />
+              <img src={item.image} alt='Image' className='accordion-image' />
             </div>
           </div>
         )}
@@ -182,123 +191,126 @@ const Comments = () => {
   return (
     <div>
       <Navbar />
-      <div className="main-header">
-        <div>
-          <div className="content">
-            <div className="comment-data-box">
-              <img className="comment-data-image" src={techImage}></img>
-              <div>
-                <a href={techLink} className="comment-tech-link">
-                  <h2>{techName}</h2>
-                </a>
-                <p className="comment-tech-description">{techDescription}</p>
-              </div>
+      <MainHeader />
+      <div className='input-container'>
+        <input type='text' className='input-bar' placeholder='Search APIs...' />
+      </div>
+
+      <div className='accordion'>{comments}</div>
+    </div>
+  );
+};
+
+const MainHeader = () => {
+  return (
+    <div className='main-header'>
+      <div>
+        <div className='content'>
+          <div className='comment-data-box'>
+            <img className='comment-data-image' src={techImage}></img>
+            <div>
+              <a href={techLink} className='comment-tech-link'>
+                <h2>{techName}</h2>
+              </a>
+              <p className='comment-tech-description'>{techDescription}</p>
             </div>
-            <button className="button" onClick={openOverlay}>
-              + ADD POST
-            </button>
-            {showOverlay && (
-              <div className="overlay-comments">
-                <div className="overlay-content-comments">
-                  <div>
-                    <form>
-                      <div className="formGroup-two">
-                        <div>
-                          <h2>Add FORM</h2>
-                          <hr className="line" />
-                          <input
-                            type="text"
-                            className="input-one-first"
-                            placeholder="Title"
-                            value={titleEntry}
-                            onChange={(event) => {
-                              setTitleEntry(event.target.value);
-                            }}
-                          />
-                          <h5></h5>
-                          <input
-                            type="text"
-                            className="input-one-c"
-                            placeholder="Language Used"
-                            // required
-                            value={languageEntry}
-                            onChange={(event) => {
-                              setLanguageEntry(event.target.value);
-                            }}
-                          />
-                          <Editor
-                            apiKey="ba2mzqsjqzq6lv0fu4numgypg3j9125otxy4rpzttx7vji3q"
-                            initialValue={initialVal}
-                            className="custom-editor"
-                            onEditorChange={handleEditorChange}
-                            value={entry}
-                            onChange={(event) => {
-                              console.log(event.target.value);
-                              setEntry(event.target.value);
-                            }}
-                            init={{
-                              height: 300,
-                              max_height: 340,
-                              menubar: true,
-                              plugins: [
-                                'advlist autolink lists link image',
-                                'charmap print preview anchor help',
-                                'searchreplace visualblocks code',
-                                'insertdatetime media table paste wordcount',
-                              ],
-                              toolbar:
-                                'undo redo | formatselect | bold italic | \
-                                alignleft aligncenter alignright alignjustify | \
-                                bullist numlist outdent indent | link image',
-                              content_style:
-                                'body { font-family: Arial, sans-serif; font-size: 14px }',
-                            }}
-                          />
-                          <input
-                            type="file"
-                            className="input-one-image"
-                            accept="image/*"
-                            value={image}
-                            onChange={(event) => {
-                              setImage(event.target.value);
-                            }}
-                          />
-                        </div>
-                        <div className="btn">
-                          <button
-                            type="submit"
-                            className="login-button"
-                            onClick={addComment}
-                          >
-                            Submit
-                          </button>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
+          </div>
+          <button className='button' onClick={openOverlay}>
+            + ADD POST
+          </button>
+          {showOverlay && (
+            <div className='overlay-comments'>
+              <div className='overlay-content-comments'>
+                <div>
+                  <Form />
                 </div>
               </div>
-            )}
-          </div>
+            </div>
+          )}
         </div>
       </div>
-
-      <div className="input-container">
-        <input type="text" className="input-bar" placeholder="Search APIs..." />
-      </div>
-
-      <div className="accordion">{comments}</div>
     </div>
+  );
+};
+
+const Form = () => {
+  return (
+    <form>
+      <div className='formGroup-two'>
+        <div>
+          <h2>Add FORM</h2>
+          <hr className='line' />
+          <input
+            type='text'
+            className='input-one-first'
+            placeholder='Title'
+            value={titleEntry}
+            onChange={event => {
+              setTitleEntry(event.target.value);
+            }}
+          />
+          <h5></h5>
+          <input
+            type='text'
+            className='input-one-c'
+            placeholder='Language Used'
+            // require
+            value={languageEntry}
+            onChange={event => {
+              setLanguageEntry(event.target.value);
+            }}
+          />
+          <Editor
+            apiKey='ba2mzqsjqzq6lv0fu4numgypg3j9125otxy4rpzttx7vji3q'
+            initialValue={initialVal}
+            className='custom-editor'
+            onEditorChange={handleEditorChange}
+            value={entry}
+            onChange={event => {
+              console.log(event.target.value);
+              setEntry(event.target.value);
+            }}
+            init={{
+              height: 300,
+              max_height: 340,
+              menubar: true,
+              plugins: [
+                'advlist autolink lists link image',
+                'charmap print preview anchor help',
+                'searchreplace visualblocks code',
+                'insertdatetime media table paste wordcount',
+              ],
+              toolbar:
+                'undo redo | formatselect | bold italic | \
+              alignleft aligncenter alignright alignjustify | \
+              bullist numlist outdent indent | link image',
+              content_style:
+                'body { font-family: Arial, sans-serif; font-size: 14px }',
+            }}
+          />
+          <input
+            type='file'
+            className='input-one-image'
+            accept='image/*'
+            value={image}
+            onChange={event => {
+              setImage(event.target.value);
+            }}
+          />
+        </div>
+        <div className='btn'>
+          <button type='submit' className='login-button' onClick={addComment}>
+            Submit
+          </button>
+        </div>
+      </div>
+    </form>
   );
 };
 
 export default Comments;
 
-
-
-
 //this was our mock data before working with the database
-
 
 // const data = [
 //   {
