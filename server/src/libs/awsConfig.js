161fc3b028e06e60b11b4791a3ece1a8df1516d0
Clayton Stewart <claytons1212@gmail.com>
2023-07-20 14:26:22 -0600

    finalized s3 uploads

--- server/src/libs/awsConfig.js
+++ server/src/libs/awsConfig.js
@@ -1,9 +1,10 @@
 const { S3Client } = require('@aws-sdk/client-s3');
+const secrets = require('./secrets.js');
 // Set the AWS Region.
 const config = {
   region: 'us-east-1',
-  accessKeyId: 'AKIAVKLTZQOTX2AGXIFE',
-  secretAccessKey: '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',
+  accessKeyId: secrets.accessKeyId,
+  secretAccessKey: secrets.secretAccessKey,
 };
 // Create an Amazon S3 service client object.
 const s3 = new S3Client(config);
